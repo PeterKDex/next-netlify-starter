cd61973b2aefb176c58ad821fd3777fcd2d0befe
PeterKDex <89813318+PeterKDex@users.noreply.github.com>
2021-11-05 18:20:12 -0700

    Clarify demo description.

--- pages/ssg/[path].tsx
+++ pages/ssg/[path].tsx
@@ -22,9 +22,9 @@ const DataGenerated:NextPage<{ path: string, clock: Clock }> = (props) => {
       <main>
         <Header title={ `Static site generated - ${props.path}` } />
         <h2>{ props.clock.timeValue }</h2>
-        <p>For a static page this will show server clock at <em>site build time</em>.</p>
-        <p>For a dynamic fallback page this will show server clock at <em>page first request time</em>.</p>
-        <p>In either case the pages and clock will not update for current deployment.</p>
+        <p>For a static page this will show server clock at <em>site build time</em> during deployment.</p>
+        <p>For a dynamic fallback page this will show server clock at <em>page first request time</em>. Note: this happens the when anyone loads this page, or the home page due to link prefetching.</p>
+        <p>In either case the content will not update again until a new site build/deploy.</p>
       </main>
 
       <Footer />
